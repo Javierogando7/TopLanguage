a187882014e93984d15cc53fd1cfec9a5b80c7d3
Javier Ogando <javierogando7@gmail.com>
2019-02-10 23:43:38 -0400

    Resolve merge conflit by incorporating both changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login'
-=======
 import { NavController, App } from 'ionic-angular';
+import { LoginPage } from '../login/login'
 
->>>>>>> 426c107ee41d747156e507d2c1592aecf01d61d2
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -15,17 +11,14 @@ export class HomePage {
   constructor(public navCtrl: NavController, public app: App) {
 
   }
-<<<<<<< HEAD
   GoLogin(){
     this.navCtrl.push(LoginPage);
-=======
-
+  }
   loadSetting(){
     //this.app.getRootNav().push(SettingPage);
   }
 
   loadSignOut(){
     //this.app.getRootNav().setRoot(LoginPage);
->>>>>>> 426c107ee41d747156e507d2c1592aecf01d61d2
   }
 }
