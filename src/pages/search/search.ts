9fdf6ab8af24a2263b67d7ab2f1fd01e2cbe4785
Javier Ogando <javierogando7@gmail.com>
2019-03-20 22:12:59 -0400

    Agrega la funcion de busqueda

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,6 @@
+import { DatabaseProvider } from './../../providers/database/database';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 
 
 @IonicPage()
@@ -8,13 +8,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-search',
   templateUrl: 'search.html',
 })
-export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class SearchPage {
+  words = [];
+  items = []; 
+  searchTerm = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public sql: DatabaseProvider) {
+    
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+    this.items = this.sql.getTable('favorite');
+    this.setFilteredItems();
   }
 
+  filterItems(searchTerm){
+    return this.items.filter((item) => {
+        return item.englishWord.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });     
+  }
+
+  setFilteredItems() {
+
+    this.words = this.filterItems(this.searchTerm);
+
+  }
 }
